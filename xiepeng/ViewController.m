fab943ca01cfa4f43bb955ba81b2831d7a20e1fa
a-do <13671834235@163.com>
2016-02-25 15:21:28 +0800

    静态单元格的使用

---
+++ xiepeng/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  xiepeng
+//
+//  Created by 谢谦 on 16/2/25.
+//  Copyright © 2016年 杜苏南. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
